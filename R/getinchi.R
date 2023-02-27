94859654c0388f9ff401f22e8df2e97a3f8609a3
Ahusha Chimmiri <achimmiri@ipb-halle.de>
2023-02-27 13:21:32 +0100

    Get inchi from CAS number

---
+++ R/getinchi.R
@@ -0,0 +1,16 @@
+getinchi<-function(getCAS)
+{
+  ###CAS: 328-50-7
+  getCAS1<-stringr::str_replace(getCAS,pattern='CAS:',replacement ="")
+  getCAS2<-stringr::str_trim(getCAS1)
+  ################################
+  url<- "http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"
+  out<-tryCatch({jsonlite::fromJSON(paste0(url,getCAS2, "/property/InChIKey"))}, error = function(x) {return(NA)})
+  out1<-tryCatch({jsonlite::fromJSON(paste0(url,getCAS2, "/property/CanonicalSMILES"))}, error = function(x) {return(NA)})
+  out2<-tryCatch({jsonlite::fromJSON(paste0(url,getCAS2, "/property/InChI"))}, error = function(x) {return(NA)})
+  #################################https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/
+  OIN<-tryCatch({out2$PropertyTable$Properties$InChI},error=function(cond){return(NA)})
+  ################################
+  return(tryCatch({OIN[1]},error=function(cond){return(NA)}))
+}
+
